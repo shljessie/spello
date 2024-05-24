d4bdc7f64ad376057df280a989fd1c623011e396
shljessie <sl994@cornell.edu>
2024-05-23 21:27:20 -0700

    spello initial commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+// src/App.js
+
+import './index.css';
+
+import React from 'react';
+import { Tldraw } from 'tldraw';
+
+function App() {
+  return (
+    <div style={{ position: 'fixed', inset: 0 }}>
+      <Tldraw />
+    </div>
+  );
+}
+
+export default App;
